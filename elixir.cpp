c395cdb24ae898e58d039453817751a7410a4538
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-01-25 10:27:36 +0530

    refactor: updated clear_bit macro and added pop_bit function

--- elixir.cpp
+++ elixir.cpp
@@ -4,10 +4,12 @@
 #include "src/bits.h"
 
 int main() {
-    U64 b = 0x8000000000000001ULL;
+    U64 b = 0x8000000000000002ULL;
     print_bitboard(b);
     std::cout << "Number of bits: " << count_bits(b) << "\n";
     std::cout << "MSB index: " << msb_index(b) << "\n";
     std::cout << "LSB index: " << lsb_index(b) << "\n";
+    std::cout << "LSB index: " << pop_bit(b) << "\n";
+    print_bitboard(b);
     return 0;
 }
--- src/bits.h
+++ src/bits.h
@@ -8,7 +8,7 @@
 #define bit(sq) (1ULL << (sq))
 #define get_bit(bb, sq) (bb & bit(sq))
 #define set_bit(bb, sq) (bb |= bit(sq))
-#define pop_bit(bb, sq) (bb &= ~bit(sq))
+#define clear_bit(bb, sq) (bb &= ~bit(sq))
 #define flip_bit(bb, sq) (bb ^= bit(sq))
 
 inline int count_bits(U64 bb) { 
@@ -19,9 +19,16 @@ inline int lsb_index(U64 bb) {
     assert(bb);
     return std::countr_zero(bb); 
 }
+
 inline int msb_index(U64 bb) { 
     assert(bb);
     return 63 - std::countl_zero(bb); 
+}
+
+inline int pop_bit(U64 &bb) {
+    int index = lsb_index(bb);
+    clear_bit(bb, index);
+    return index;
 }
 
 void print_bitboard(U64 bitboard);
