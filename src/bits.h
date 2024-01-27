51e660550ca5ba5767c96b2c4f629baad5abbd27
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-01-27 11:42:08 +0530

    rrefactor: minor refactoring

--- src/bits.h
+++ src/bits.h
@@ -6,55 +6,53 @@
 #include "types.h"
 #include "defs.h"
 
-namespace elixir::bits {
-
-    inline U64 bit(Square sq) { 
-        assert(sq != Square::no_sq);
-        return 1ULL << static_cast<int>(sq); 
-    }
-
-    inline int get_bit(U64 bb, Square sq) { 
-        assert(sq != Square::no_sq && bb);
-        return bb & bit(sq); 
-    }
-
-    inline void set_bit(U64 &bb, Square sq) { 
-        assert(sq != Square::no_sq && bb);
-        bb |= bit(sq); 
-    }
-
-    inline void clear_bit(U64 &bb, Square sq) { 
-        assert(sq != Square::no_sq && bb);
-        bb &= ~bit(sq); 
-    }
-
-    inline void flip_bit(U64 &bb, Square sq) { 
-        assert(sq != Square::no_sq && bb);
-        bb ^= bit(sq); 
-    }
-
-    inline int count_bits(U64 bb) { 
-        return std::popcount(bb); 
-    }
-
-    inline int lsb_index(U64 bb) { 
-        assert(bb);
-        return std::countr_zero(bb); 
-    }
-
-    inline int msb_index(U64 bb) { 
-        assert(bb);
-        return 63 - std::countl_zero(bb); 
-    }
-
-    inline int pop_bit(U64 &bb) {
-        assert(bb);
-        int index = lsb_index(bb);
-        clear_bit(bb, static_cast<Square>(index));
-        return index;
-    }
-}
-
 namespace elixir {
+    namespace bits {
+        inline U64 bit(Square sq) { 
+            assert(sq != Square::no_sq);
+            return 1ULL << static_cast<int>(sq); 
+        }
+
+        inline int get_bit(U64 bb, Square sq) { 
+            assert(sq != Square::no_sq && bb);
+            return bb & bit(sq); 
+        }
+
+        inline void set_bit(U64 &bb, Square sq) { 
+            assert(sq != Square::no_sq && bb);
+            bb |= bit(sq); 
+        }
+
+        inline void clear_bit(U64 &bb, Square sq) { 
+            assert(sq != Square::no_sq && bb);
+            bb &= ~bit(sq); 
+        }
+
+        inline void flip_bit(U64 &bb, Square sq) { 
+            assert(sq != Square::no_sq && bb);
+            bb ^= bit(sq); 
+        }
+
+        inline int count_bits(U64 bb) { 
+            return std::popcount(bb); 
+        }
+
+        inline int lsb_index(U64 bb) { 
+            assert(bb);
+            return std::countr_zero(bb); 
+        }
+
+        inline int msb_index(U64 bb) { 
+            assert(bb);
+            return 63 - std::countl_zero(bb); 
+        }
+
+        inline int pop_bit(U64 &bb) {
+            assert(bb);
+            int index = lsb_index(bb);
+            clear_bit(bb, static_cast<Square>(index));
+            return index;
+        }
+    }
     extern void print_bitboard(U64 bitboard);
 }
--- src/types.h
+++ src/types.h
@@ -7,9 +7,9 @@ namespace elixir {
     using Bitboard = std::uint64_t;
 
     using U8 = std::uint8_t;
-	using U16 = std::uint16_t;
-	using U32 = std::uint32_t;
-	using U64 = std::uint64_t; 
+    using U16 = std::uint16_t;
+    using U32 = std::uint32_t;
+    using U64 = std::uint64_t; 
 
     using I8 = std::int8_t;   
 }
