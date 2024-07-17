65aca981d893e4d78b33033cb96516848e1fc107
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-07-17 19:22:24 +0530

    fix: fix singular extension
    Bench: 354319

--- src/ss.h
+++ src/ss.h
@@ -1,15 +1,17 @@
 #pragma once
 
 #include "defs.h"
-#include "types.h"
 #include "move.h"
+#include "types.h"
+
 
 namespace elixir::search {
     struct SearchStack {
-        move::Move move       = move::NO_MOVE;
-        move::Move killers[2] = {};
-        int eval = -INF;
+        move::Move move          = move::NO_MOVE;
+        move::Move excluded_move = move::NO_MOVE;
+        move::Move killers[2]    = {};
+        int eval                 = -INF;
+        ContHistEntry *cont_hist = nullptr;
         int ply;
-        ContHistEntry* cont_hist = nullptr;
     };
 }
