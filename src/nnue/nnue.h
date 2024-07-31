1644da15b064895d9e24d43b0e781515cb8b3712
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-07-31 17:48:52 +0530

    chore: updated nn reset function

--- src/nnue/nnue.h
+++ src/nnue/nnue.h
@@ -23,8 +23,8 @@ namespace elixir {
                 int current_acc;
 
                 void reset() {
-                    accumulators.assign(accumulators.size(), Accumulator());
                     current_acc = 0;
+                    accumulators[current_acc] = Accumulator();
                 }
 
                 NNUE() {
