b2d2cc3da8bec88c16634383973a619c96f0417c
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-08-02 12:24:04 +0530

    feat: added improving heuristics to LMR
    Bench: 329081

--- src/search.cpp
+++ src/search.cpp
@@ -421,6 +421,7 @@ namespace elixir::search {
             R -= (is_quiet_move ? history_score / HISTORY_GRAVITY : 0);
             R -= board.is_in_check();
             R += cutnode;
+            R += !improving;
             
             if (depth > 1 && legals > 1) {
                 R = std::clamp(R, 1, new_depth);
