7911b2bc42f8ff560a67d85f10912d3676aa39f7
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-08-19 17:14:58 +0530

    feat: reduce more if tt move is present
    Bench: 3267208

--- src/search.cpp
+++ src/search.cpp
@@ -435,6 +435,7 @@ namespace elixir::search {
             R -= board.is_in_check();
             R += cutnode;
             R -= tt_pv;
+            R += bool(tt_move);
             
             if (depth > 1 && legals > 1) {
                 R = std::clamp(R, 1, new_depth);
