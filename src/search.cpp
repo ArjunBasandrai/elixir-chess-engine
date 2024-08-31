9d42d542da96053750ab77ab8a545b395e6bfc97
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-08-31 15:16:07 +0530

    feat: updated rfp return value
    Bench: 4819829

--- src/search.cpp
+++ src/search.cpp
@@ -301,7 +301,7 @@ namespace elixir::search {
             | confident that we will not fall below beta anytime soon, then we cutoff.     |
             */
             if (depth <= RFP_DEPTH && eval - futility_margin(depth, improving, cutnode, tt_hit) >= beta && beta >= -MATE_FOUND && eval <= MATE_FOUND) {
-                return (eval + beta) / 2;
+                return std::lerp(beta, eval, (improving ? 0.50 : 0.35));
             }
 
             /*
