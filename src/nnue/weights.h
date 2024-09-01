1712c22afcde03e4264be1180acdb0416a656642
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-09-01 21:18:40 +0530

    Avx512 (#132)
    Bench: 5738653

--- src/nnue/weights.h
+++ src/nnue/weights.h
@@ -5,9 +5,9 @@
 
 namespace elixir::nnue {
     struct Network {
-        alignas(32) I16 layer_1_weights[INPUT_WEIGHTS][HIDDEN_SIZE];
-        alignas(32) I16 layer_1_biases[HIDDEN_SIZE];
-        alignas(32) I16 output_weights[OUTPUT_BUCKETS][2][HIDDEN_SIZE];
-        alignas(32) I16 output_bias[OUTPUT_BUCKETS];
+        alignas(64) I16 layer_1_weights[INPUT_WEIGHTS][HIDDEN_SIZE];
+        alignas(64) I16 layer_1_biases[HIDDEN_SIZE];
+        alignas(64) I16 output_weights[OUTPUT_BUCKETS][2][HIDDEN_SIZE];
+        alignas(64) I16 output_bias[OUTPUT_BUCKETS];
     };
 }
