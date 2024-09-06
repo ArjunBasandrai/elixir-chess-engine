19999ad7fef5fa644780217e52ceebbdd8b49937
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-09-07 01:32:49 +0530

    feat: more lmr when worsening
    Bench: 4224660

--- src/search.cpp
+++ src/search.cpp
@@ -446,6 +446,7 @@ namespace elixir::search {
             R -= board.is_in_check();
             R += cutnode;
             R -= tt_pv;
+            R += worsening;
             
             if (depth > 1 && legals > 1) {
                 R = std::clamp(R, 1, new_depth);
