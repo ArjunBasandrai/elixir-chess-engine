1f44fadd32724f6f9e0bd574f25ebb50941e116d
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-09-09 01:31:25 +0530

    feat: do double extension only in non pv nodes
    Bench: 5219701

--- src/search.cpp
+++ src/search.cpp
@@ -409,7 +409,7 @@ namespace elixir::search {
 
                 if (s_score < s_beta) {
                     const int double_margin = 300 * pv_node;
-                    extensions += 1 + (s_score < s_beta - double_margin);
+                    extensions += 1 + (!pv_node && s_score < s_beta - double_margin);
                 }
 
                 else if (s_beta >= beta) 
