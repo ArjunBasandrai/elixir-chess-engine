10de6e7aa0a681a2d672bd9dcd84471674106dc0
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-09-09 01:55:03 +0530

    fix: use static eval
    Bench: 5157990

--- src/search.cpp
+++ src/search.cpp
@@ -181,7 +181,6 @@ namespace elixir::search {
         bool root_node = ss->ply == 0;
         bool pv_node   = ((beta - alpha > 1) || root_node);
         bool in_check  = board.is_in_check();
-        int eval;
 
         if (! root_node && (time_manager.should_stop(info) || info.stopped))
             return 0;
@@ -259,29 +258,34 @@ namespace elixir::search {
 
         if (!ss->excluded_move) {
             if (in_check)
-                eval = ss->eval = SCORE_NONE;
+                ss->static_eval = ss->eval = SCORE_NONE;
 
-            else
-                eval = ss->eval = (tt_hit && can_use_tt_score) ? result.score : board.evaluate();
+            else {
+                const bool should_use_tt_score = (tt_flag == TT_EXACT
+						|| tt_flag == TT_ALPHA && result.score < ss->static_eval
+						|| tt_flag == TT_BETA  && result.score > ss->static_eval);
+                ss->static_eval = board.evaluate();
+                ss->eval = (tt_hit && should_use_tt_score) ? result.score : ss->static_eval;
+            }
         }
 
         const bool improving = [&] {
             if (in_check)
                 return false;
-            if ((ss - 2)->eval != SCORE_NONE)
-                return ss->eval > (ss - 2)->eval;
-            if ((ss - 4)->eval != SCORE_NONE)
-                return ss->eval > (ss - 4)->eval;
+            if ((ss - 2)->static_eval != SCORE_NONE)
+                return ss->static_eval > (ss - 2)->static_eval;
+            if ((ss - 4)->static_eval != SCORE_NONE)
+                return ss->static_eval > (ss - 4)->static_eval;
             return true;
         }();
 
         const bool worsening = [&] {
             if (in_check)
                 return true;
-            if ((ss - 1)->eval != SCORE_NONE)
-                return ss->eval < (ss - 1)->eval;
-            if ((ss - 3)->eval != SCORE_NONE)
-                return ss->eval < (ss - 3)->eval;
+            if ((ss - 1)->static_eval != SCORE_NONE)
+                return ss->static_eval < (ss - 1)->static_eval;
+            if ((ss - 3)->static_eval != SCORE_NONE)
+                return ss->static_eval < (ss - 3)->static_eval;
             return false;
         }();
 
@@ -290,7 +294,7 @@ namespace elixir::search {
             | Razoring (~4 ELO) : If out position is way below alpha, do a verification |
             | quiescence search, if we still cant exceed alpha, then we cutoff.         |
             */
-            if (depth <= RAZOR_DEPTH && eval + RAZOR_MARGIN * depth < alpha) {
+            if (depth <= RAZOR_DEPTH && ss->eval + RAZOR_MARGIN * depth < alpha) {
                 const int razor_score = qsearch(td, alpha, beta, local_pv, ss);
                 if (razor_score <= alpha) {
                     return razor_score;
@@ -300,16 +304,16 @@ namespace elixir::search {
             | Reverse Futility Pruning (~45 ELO) : If our position is so good, that we are |
             | confident that we will not fall below beta anytime soon, then we cutoff.     |
             */
-            if (depth <= RFP_DEPTH && eval - futility_margin(depth, improving, cutnode, tt_hit) >= beta && beta >= -MATE_FOUND && eval <= MATE_FOUND) {
-                return (eval + beta) / 2;
+            if (depth <= RFP_DEPTH && ss->eval - futility_margin(depth, improving, cutnode, tt_hit) >= beta && beta >= -MATE_FOUND && ss->eval <= MATE_FOUND) {
+                return (ss->eval + beta) / 2;
             }
 
             /*
             | Null Move Pruning (~60 ELO) : If our position is so good, we give our |
             | opponent an extra move to see if we are still better.                 |
             */
-            if (depth >= NMP_DEPTH && (ss - 1)->move && eval >= beta && board.has_non_pawn_material()) {
-                int R = NMP_BASE_REDUCTION + depth / NMP_DIVISOR + std::min((eval - beta) / 200, 6) + std::min(board.get_phase(), 24) / 8;
+            if (depth >= NMP_DEPTH && (ss - 1)->move && ss->eval >= beta && board.has_non_pawn_material()) {
+                int R = NMP_BASE_REDUCTION + depth / NMP_DIVISOR + std::min((ss->eval - beta) / 200, 6) + std::min(board.get_phase(), 24) / 8;
                 R     = std::min(R, depth);
 
                 /*
