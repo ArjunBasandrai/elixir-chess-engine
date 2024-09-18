d13a15e31aa06f05855d927936065e7fdc2de9d7
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-09-19 01:58:18 +0530

    feat: added 4-ply continuation history
    Bench: 5926741

--- src/search/history/history.h
+++ src/search/history/history.h
@@ -22,7 +22,7 @@ namespace elixir {
             return 2 * quiet_history.get_quiet_history(move.get_from(), move.get_to()) +
                    (continuation_history.get_chs(move, ss - 1) +
                     continuation_history.get_chs(move, ss - 2) +
-                    continuation_history.get_chs(move, ss - 2));
+                    continuation_history.get_chs(move, ss - 4));
         }
 
         QuietHistory quiet_history;
