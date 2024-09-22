f42a8ce9427ac6a143926d97e6e41209601fc369
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-09-23 00:50:09 +0530

    TT static eval (#148)
    Elo   | 3.81 +- 4.07 (95%)
    SPRT  | 8.0+0.08s Threads=4 Hash=64MB
    LLR   | 2.89 (-2.25, 2.89) [-5.00, 0.00]
    Games | N: 6836 W: 1257 L: 1182 D: 4397
    Penta | [18, 721, 1870, 786, 23]
    https://chess.aronpetkovski.com/test/4354/
    Bench: 5406676

--- src/core/tt/tt.h
+++ src/core/tt/tt.h
@@ -15,26 +15,30 @@ namespace elixir {
     struct TTEntry {
         U64 key         = 0ULL;
         I16 score       = 0;
+        I16 static_eval = 0;
         move::Move move = move::NO_MOVE;
         I8 depth        = -1;
         TTFlag flag : 6 = TT_NONE;
         bool tt_pv : 2  = false;
 
         bool operator==(const TTEntry &other) const {
-            return key == other.key && score == other.score && depth == other.depth &&
-                   flag == other.flag;
+            return key == other.key && score == other.score && static_eval == other.static_eval &&
+                   depth == other.depth && flag == other.flag;
         }
         bool operator==(const U64 &other) const { return key == other; }
     };
 
     struct ProbedEntry {
-        int score;
+        I16 score;
+        I16 static_eval;
         move::Move best_move;
         U8 depth;
         TTFlag flag : 6;
         bool tt_pv : 2;
 
-        ProbedEntry() : score(0), best_move(move::NO_MOVE), depth(0), flag(TT_NONE), tt_pv(false) {}
+        ProbedEntry()
+            : score(0), static_eval(0), best_move(move::NO_MOVE), depth(0), flag(TT_NONE),
+              tt_pv(false) {}
 
         ProbedEntry operator=(const TTEntry &entry) {
             score     = entry.score;
@@ -56,8 +60,9 @@ namespace elixir {
         ~TranspositionTable() = default;
         void clear_tt();
         void resize(U16 size);
-        void store_tt(U64 key, int score, move::Move move, U8 depth, int ply, TTFlag flag,
-                      search::PVariation pv, bool tt_pv = false, bool improving = false);
+        void store_tt(U64 key, I16 score, I16 static_eval, move::Move move, U8 depth, int ply,
+                      TTFlag flag, search::PVariation pv, bool tt_pv = false,
+                      bool improving = false);
         bool probe_tt(ProbedEntry &result, U64 key, U8 depth, int alpha, int beta);
         int correct_score(int score, int ply) const {
             return (score > MATE_FOUND) ? score - ply : (score < -MATE_FOUND) ? score + ply : score;
