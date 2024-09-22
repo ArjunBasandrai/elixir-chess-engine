c736783b1fd8ce7456e1f22deb895e8cd640cf7b
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-09-22 18:21:56 +0530

    feat: added tuned time management terms
    Bench: 5590737

--- src/core/time_manager/time_management_terms.cpp
+++ src/core/time_manager/time_management_terms.cpp
@@ -4,15 +4,15 @@ namespace elixir {
     int DEFAULT_MOVESTOGO     = -1;
     int DEFAULT_MOVE_OVERHEAD = 100;
 
-    int BASE_SCALE       = 54;
-    int INC_SCALE        = 60;
-    int MAX_BOUND_SCALE  = 76;
-    int SOFT_BOUND_SCALE = 76;
-    int HARD_BOUND_SCALE = 250;
+    int BASE_SCALE       = 45;
+    int INC_SCALE        = 58;
+    int MAX_BOUND_SCALE  = 77;
+    int SOFT_BOUND_SCALE = 77;
+    int HARD_BOUND_SCALE = 176;
 
-    int MOVE_STABILITY_SCALE_1 = 243;
-    int MOVE_STABILITY_SCALE_2 = 135;
-    int MOVE_STABILITY_SCALE_3 = 109;
-    int MOVE_STABILITY_SCALE_4 = 88;
-    int MOVE_STABILITY_SCALE_5 = 68;
+    int MOVE_STABILITY_SCALE_1 = 233;
+    int MOVE_STABILITY_SCALE_2 = 138;
+    int MOVE_STABILITY_SCALE_3 = 104;
+    int MOVE_STABILITY_SCALE_4 = 85;
+    int MOVE_STABILITY_SCALE_5 = 69;
 }
