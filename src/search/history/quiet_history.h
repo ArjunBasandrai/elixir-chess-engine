9956a8afa908232ab873f128b71a076b90205174
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-11-11 00:08:59 +0530

    feat: made the quiet history bigger (#155)
    Elo   | 3.11 +- 2.20 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=8MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 26346 W: 5486 L: 5250 D: 15610
    Penta | [123, 3007, 6684, 3229, 130]
    https://chess.aronpetkovski.com/test/5878/
    
    Bench: 3616285

--- src/search/history/quiet_history.h
+++ src/search/history/quiet_history.h
@@ -7,15 +7,15 @@ namespace elixir {
     class QuietHistory {
       public:
         QuietHistory() {
-            history.resize(64);
+            history.resize(12);
             clear();
         }
         ~QuietHistory() = default;
 
         void clear();
 
-        void update_history(Square from, Square to, int depth, MoveList &bad_quiets);
-        int get_quiet_history(Square from, Square to) const;
+        void update_history(const Piece piece, const Square from, const Square to, const int depth, const MoveList &bad_quiets);
+        int get_quiet_history(const Piece piece, const Square from, const Square to) const;
 
       private:
         QuietHistoryArray history;
